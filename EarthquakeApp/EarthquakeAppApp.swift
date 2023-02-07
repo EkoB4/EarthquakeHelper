f1c8576e4ed0d94f72325cfbb85acf1eb3a37d28
EkoB4 <ekinbacik@gmail.com>
2023-02-07 06:48:20 +0300

    Initial Commit

---
+++ EarthquakeApp/EarthquakeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeAppApp.swift
+//  EarthquakeApp
+//
+//  Created by Ekin Bacik on 7.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
